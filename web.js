9139596a79d7fb83c960ed6c89524da15cee8561
Ubuntu <ubuntu@ip-172-31-28-168.us-west-2.compute.internal>
2013-07-23 04:38:25 +0000

    Twitter Bootstrap

--- web.js
+++ web.js
@@ -1,19 +1,20 @@
-var express = require('express');                                                                                                                                                                               
-var fs = require('fs');                                                                                                                                                                                         
-var buf = new Buffer(256);                                                                                                                                                                                      
-var app = express.createServer(express.logger());                                                                                                                                                              
-var infile = "index.html";                                                                                                                                                                                      
+var express = require('express');                                                                                                                          
+
+var fs = require('fs');                                                                                                                                    
+
+var buf = new Buffer(256);                                                                                                                                 
+
+var app = express.createServer(express.logger());
+var infile = "index.html";                                                                                                                                 
+
 app.get('/', function(request, response) {
-    buf = fs.readFileSync(infile,'utf-8');                                                                                                                                                                      
-    response.send(buf.toString());                                                                                                                                                                              
-});                                                                                                                                                                                                             
-var port = process.env.PORT || 5000;                                                                                                                                                                           
+    buf = fs.readFileSync(infile,'utf-8');                                                                                                                 
+    response.send(buf.toString());                                                                                                                         
+});                                                                                                                                                        
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
-    console.log("Listening on " + port);                                                                                                                                                   
-}); 
-
-
-
+    console.log("Listening on " + port);
+});
 
 
 
